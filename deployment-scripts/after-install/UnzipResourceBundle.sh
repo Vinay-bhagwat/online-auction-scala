8f9529c5e1cd1e0d3588038b1dd3b427e72e34ba
Vinay Bhagwat <vinay.bhagwat@afourtech.com>
2017-08-07 18:48:04 +0530

    Added snippets

--- deployment-scripts/after-install/UnzipResourceBundle.sh
+++ deployment-scripts/after-install/UnzipResourceBundle.sh
@@ -2,4 +2,4 @@
 echo "Finally working"
 cd /home/ubuntu/Deployment_Env
 unzip  -o '*.zip' -d /home/ubuntu/Online_Auction_Scala_Deployment
-sh /home/ubuntu/Online_Auction_Scala_Deployment/deployAll.sh status
+sh /home/ubuntu/Online_Auction_Scala_Deployment/deployAll.sh start
