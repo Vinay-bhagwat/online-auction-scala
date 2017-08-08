6049dfefb25afcc712942eaa35328fec02475316
Vinay Bhagwat <vinay.bhagwat@afourtech.com>
2017-08-08 12:49:48 +0530

    Added snippets

--- deployment-scripts/after-install/UnzipResourceBundle.sh
+++ deployment-scripts/after-install/UnzipResourceBundle.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /home/ubuntu/Deployment_Env
+sbt dist
+find . -name '*.zip' -exec mv --target-directory='./' '{}' +
 unzip  -o '*.zip' -d /home/ubuntu/Online_Auction_Scala_Deployment
 cd /home/ubuntu/Online_Auction_Scala_Deployment
 sh deployAll.sh start
